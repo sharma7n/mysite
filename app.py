12bab09a30195f9302d3de6f4d22345ffd1309f3
sharma7n <sharma7n@gmail.com>
2016-08-22 21:26:36 -0500

    index page

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-boots = Bootstrap(app)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/about/")
-def about():
-    return "Implement me!"
-
-@app.route("/projects/")
-def projects():
-    return "Implement me!"
-
-@app.route("/contact/")
-def contact():
-    return "Implement me!"
+@app.route("/ashenvale/")
+def ashenvale():
+	return render_template('ashenvale.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
