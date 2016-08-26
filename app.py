2bfe1eecb013e336826f251436c12b0afad2160e
sharma7n <sharma7n@gmail.com>
2016-08-25 19:21:45 -0500

    port fix

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 from flask import Flask, render_template
 
@@ -32,4 +33,5 @@ def contact():
 	pass
 
 if __name__ == '__main__':
-	app.run()
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
